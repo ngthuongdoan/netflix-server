8d1b73b880207f26c919731f5a28267571853aae
Eddie <eddie@devx.com.au>
2022-06-12 23:36:40 +0700

    fix: remove dist

--- dist/src/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Todo = void 0;
-var todo_model_1 = __importDefault(require("./todo.model"));
-exports.Todo = todo_model_1.default;
--- dist/src/validations/common.validation.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUpcoming = exports.getTrending = void 0;
-var getTrending = {};
-exports.getTrending = getTrending;
-var getUpcoming = {};
-exports.getUpcoming = getUpcoming;
